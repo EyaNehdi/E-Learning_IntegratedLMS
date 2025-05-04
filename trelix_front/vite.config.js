68bf64e0f55945b0e8665db1a89beea8acbf9766
jihed.horchani <jihed.horchani@esprit.tn>
2025-05-04 21:52:57 +0100

    fix vite

--- trelix_front/vite.config.js
+++ trelix_front/vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss(),],
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Your backend port
+        target: 'http://localhost:5000',
         changeOrigin: true,
-        "/cdn-cgi/rum": false
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
