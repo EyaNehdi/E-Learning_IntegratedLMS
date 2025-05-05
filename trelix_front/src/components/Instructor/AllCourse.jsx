f739c42134f9a09681d1c44c0e7d8c1e9d543be6
[Jihed Lourassi] <[jihedlrs@gmail.com]>
2025-05-05 22:06:48 +0100

    fixed recommendatio

--- trelix_front/src/components/Instructor/AllCourse.jsx
+++ trelix_front/src/components/Instructor/AllCourse.jsx
@@ -314,6 +314,7 @@ function Allcourse() {
                               }
                               alt={course.title}
                             />
+
                           </a>
                           <a
                             href="#"
