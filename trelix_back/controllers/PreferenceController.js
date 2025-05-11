a05a499b5dcca2bed9ea662ec8746b0e6318e871
jihed.horchani <jihed.horchani@esprit.tn>
2025-05-11 13:52:55 +0100

    fix url

--- trelix_back/controllers/PreferenceController.js
+++ trelix_back/controllers/PreferenceController.js
@@ -189,8 +189,7 @@ const getRecommendedCourses = async (req, res) => {
     res.status(500).json({ message: "Erreur du serveur", error: error.message });
   }
 };
-const getPreferencesByUserId = async (req, res) => {
-  const { id } = req.params;
+
 
 module.exports = { createpreference, updatePreference, getAllPreference, getRecommendedCourses };
 
