921cac51c6f7b7b0d8ddfdc28273605b414ff237
yasminebouteraa <bouteraa.yassmine@esprit.tn>
2025-05-13 20:39:50 +0200

    Citation hosted

---
+++ trelix_back/controllers/citationController.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+const fetchCitation = async (req, res) => {
+  try {
+    const response = await axios.get('https://zenquotes.io/api/random');
+    const quote = response.data[0]; // Get the first quote from the array
+
+    console.log("citation", quote);
+
+    // Send only what you need
+    res.json({
+      q: quote.q,
+      a: quote.a
+    });
+  } catch (error) {
+    console.error('Error fetching quote:', error.message);
+
+    if (error.response && error.response.status === 429) {
+      return res.status(429).json({ error: 'Rate limit exceeded' });
+    }
+
+    res.status(500).json({ error: 'Failed to fetch quote' });
+  }
+};
+
+module.exports = { fetchCitation };
