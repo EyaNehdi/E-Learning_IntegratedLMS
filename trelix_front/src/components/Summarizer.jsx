d9eb954c066fbece5b5254cf8d3d0d8e0e06d868
yasminebouteraa <bouteraa.yassmine@esprit.tn>
2025-05-16 17:06:44 +0200

    fixeUI

--- trelix_front/src/components/Summarizer.jsx
+++ trelix_front/src/components/Summarizer.jsx
@@ -43,7 +43,7 @@ const Summarizer = () => {
 
   return (
     <div className="p-4 bg-white rounded shadow max-w-xl mx-auto mt-8">
-      <h2 className="text-2xl font-bold mb-4">Summarize od PDF 📚</h2>
+      <h2 className="text-2xl font-bold mb-4">Summarize your PDF 📚</h2>
       <input 
         type="file" 
         accept="application/pdf"
@@ -54,7 +54,7 @@ const Summarizer = () => {
         onClick={summarizePdf}
         className="bg-blue-500 text-white px-4 py-2 rounded"
       >
-        Summarize Your PDF
+        Generate PDF
       </button>
 
       {loading && <p className="mt-2">loading...</p>}
